b26ded17096311f273bdbeced45d33bc7a663e55
lucas <lucassdsilvadev@gmail.com>
2026-08-04 21:15:32 -0300

    A

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Exercicio 1
+// Questão 1
+var a = 10
+var b = 6
+console.log(a + b);
+
+// Questão 2
+var nome = "Lucas"
+var idade = 23
+console.log("Meu nome é", nome , "e tenho", idade , "anos.")
+
+// Questão 3
+var a = 8
+var b = 7
+var c = 9
+console.log( (a + b + c ) / 3 )
+
+//Questão 4
+
+
+// Questão 5
+let idade = 18;
+if (idade >=18)
+console.log ()
