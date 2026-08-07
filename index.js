2de56b94133f659ac7fb659586eec9b2b42e3b97
Lucas <lucassdsilvadev@gmail.com>
2026-08-06 21:21:36 -0300

    empacado na Q.12

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+//Questão 1
 console.log("Questão 1")
 var a = 10
 var b = 6
 console.log(a + b);
-
+//Questão 2
 console.log("Questão 2")
 var nome = "Lucas"
 var year = 23
 console.log("Meu nome é", nome, "e tenho", year, "anos.")
-
+//Questão 3
 console.log("Questão 3")
 var a = 8
 var b = 7
 var c = 9
 console.log((a + b + c) / 3)
-
+//Questão 4
 console.log("Questão 4")
 let numero = 5;
 
@@ -23,7 +24,7 @@ if (numero % 2 === 0) {
 else {
     console.log("número é impar");
 }
-
+//Questão 5
 console.log("Questão 5")
 let idade = 20;
 
@@ -33,7 +34,7 @@ if (idade >= 18) {
 else{
     console.log("Menor de idade");
 }
-
+//Questão 6
 console.log("Questão 6")
 let nota = 5;
 
@@ -46,38 +47,50 @@ console.log("Nota C");
 } else if (nota <= 4) {
     console.log("Nota D");
 } 
-
+//Questão 7
 console.log("Questão 7")
 for(let i = 1; i <= 10; i++) {
     console.log(i);
 }
-
+//Questão 8
 console.log("Questão 8")
 for(let i = 1; i <= 10; i++) {
     console.log(i*5);
 }
-
+//Questão 9
 console.log("Questão 9")
 for(let i = 1; i <= 100; i++) {
     let soma = 0
     soma = soma + i
     console.log(soma);
 }
-
+//Questão 10
 console.log("Questão 10")
 function dobrar(x) {
     return x * 2
 }
 console.log(dobrar(10));
-//
+//Questão 11
 console.log("Questão 11")
 function calcularIMC(x,y) {
     var IMC = x / (y*y)
     return IMC
 }
 console.log(calcularIMC(70,1.75).toFixed(2));
-//
+//Questão 12
 console.log("Questão 12")
+function saudacao(nome,hora) {
 
-//
+    if (hora >=6) {
+        console.log("Bom dia!, ");
+    } else if(hora >=13){
+        console.log("Boa Tarde!, ");
+    }else if(hora <=18)
+    console.log("Boa Noite!, ");
+
+return
+}
+console.log(saudacao("Lucas",6))
+
+//Questão 13
 console.log("Questão 13")
